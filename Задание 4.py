62d37ba998121a9dc21c1be1d5584cb7f8267104
andboytsov <157279710+andboytsov@users.noreply.github.com>
2024-05-26 17:05:23 +0300

    Add files via upload

---
+++ Задание 4.py
@@ -0,0 +1,28 @@
+import datetime as dt
+
+UTC_OFFSET = {
+    'Санкт-Петербург': 3,
+    'Москва': 3,
+    'Самара': 4,
+    'Новосибирск': 7,
+    'Екатеринбург': 5,
+    'Нижний Новгород': 3,
+    'Казань': 3,
+    'Челябинск': 5,
+    'Омск': 6,
+    'Ростов-на-Дону': 3,
+    'Уфа': 5,
+    'Красноярск': 7,
+    'Пермь': 5,
+    'Воронеж': 3,
+    'Волгоград': 3,
+    'Краснодар': 3,
+    'Калининград': 2
+}
+
+def what_time(city):
+    utc_offset = UTC_OFFSET[city]
+    current_time = dt.datetime.16:40()
+    local_time = current_time + dt.timedelta(hours=utc_offset)
+    return local_time.strftime("%H:%M")
+print(what_time('Екатеринбург'))
